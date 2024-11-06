2277bc44930e8eb08bd9c36e6a576f7c9a1540c9
Kaleb K <kaleb.tahji.k@gmail.com>
2024-11-06 13:02:56 -0600

    Frequency Bands

--- beat-detection/Assets/Scripts/AudioFXScripts/AudioPeer.cs
+++ beat-detection/Assets/Scripts/AudioFXScripts/AudioPeer.cs
@@ -6,22 +6,49 @@ using UnityEngine;
 public class AudioPeer : MonoBehaviour
 {
     private AudioSource _audioSource;
+    private AudioClip _audioClip;
+    
     public static float[] samples = new float[512];
+    public static float[] freqBand = new float[8];
     
     // Start is called before the first frame update
     void Start()
     {
         _audioSource = GetComponent<AudioSource>();
+        _audioClip = _audioSource.clip;
     }
 
     // Update is called once per frame
     void Update()
     {
         GetSpectrumAudioSource();
-    }
+        MakeFrequencyBands(); }
 
     void GetSpectrumAudioSource()
     {
         _audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void MakeFrequencyBands()
+    {
+        int count = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+            freqBand[i] = average * 10;
+        }
     }
 }
